e3b19dfeb1c8ae9237622f426d0234df268e11ac
Leandro Cobra <leandroccobra@gmail.com>
2026-03-20 13:22:23 -0400

    método de autenticar

--- src/service/AutService.java
+++ src/service/AutService.java
@@ -1,4 +1,30 @@
 package service;
 
+import model.Usuario;
+import repository.UsuarioRepository;
+
 public class AutService {
+
+    private UsuarioRepository usuarioRepository;
+
+    public AutService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuario autenticar(String usuario, String senha) {
+        Usuario usuarioEncontrado = usuarioRepository.buscarPorUsuario(usuario);
+        if (usuarioEncontrado == null) {
+            return null;
+        }
+
+        if (!usuarioEncontrado.getSenha().equals(senha)) {
+            return null;
+        }
+
+        if (!usuarioEncontrado.isAtivo()) {
+            return null;
+        }
+
+        return usuarioEncontrado;
+    }
 }
