12718e0ff774f92d99cd0238c3e3d6de06f2321f
Leandro Cobra <leandroccobra@gmail.com>
2026-03-20 16:15:57 -0400

    método para adicionar item ao pedido criado

--- src/service/PedidoService.java
+++ src/service/PedidoService.java
@@ -1,19 +1,29 @@
 package service;
 
+import model.ItemPedido;
 import model.Mesa;
 import model.Pedido;
+import model.Produto;
 import model.enums.StatusMesa;
+import model.enums.StatusPagamentoPedido;
+import repository.ItemPedidoRepository;
 import repository.MesaRepository;
 import repository.PedidoRepository;
+import repository.ProdutoRepository;
 
 public class PedidoService {
 
     private MesaRepository mesaRepository;
     private PedidoRepository pedidoRepository;
+    private ProdutoRepository produtoRepository;
+    private ItemPedidoRepository itemPedidoRepository;
 
-    public PedidoService(MesaRepository mesaRepository, PedidoRepository pedidoRepository) {
+    public PedidoService(MesaRepository mesaRepository, PedidoRepository pedidoRepository,
+                         ProdutoRepository produtoRepository, ItemPedidoRepository itemPedidoRepository) {
         this.mesaRepository = mesaRepository;
         this.pedidoRepository = pedidoRepository;
+        this.produtoRepository = produtoRepository;
+        this.itemPedidoRepository = itemPedidoRepository;
     }
 
     public Pedido abrirPedido(int numeroMesa) {
@@ -39,6 +49,43 @@ public class PedidoService {
         pedidoRepository.salvar(novoPedido);
 
         return novoPedido;
+    }
+
+    public ItemPedido adicionarItemAoPedido(int numeroMesa, int produtoId, int quantidade) {
+
+        Pedido pedido = pedidoRepository.buscarPedidoAtivoPorMesa(
+                mesaRepository.buscarPorNumero(numeroMesa).getId());
+
+        if (pedido == null) {
+            return null;
+        }
+
+        if (pedido.getStatusPagamento() != StatusPagamentoPedido.ABERTO) {
+            return null;
+        }
+
+        Produto produto = produtoRepository.buscarPorId(produtoId);
+
+        if (produto == null) {
+            return null;
+        }
+
+        if ((!produto.isAtivo())) {
+            return null;
+        }
+
+        if (quantidade <= 0) {
+            return null;
+        }
+
+        int novoId = itemPedidoRepository.listarTodos().size() + 1;
+
+        ItemPedido novoItem = new ItemPedido(novoId, pedido.getId(), produto.getId(),
+                quantidade, produto.getPreco());
+
+        itemPedidoRepository.salvar(novoItem);
+
+        return novoItem;
     }
 }
 
