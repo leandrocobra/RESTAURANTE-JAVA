2c061cbee4f7b8fdb555d4c3ae0330f1a874d6b6
Leandro Cobra <leandroccobra@gmail.com>
2026-03-20 13:57:49 -0400

    método para abrir pedido criado

--- src/service/PedidoService.java
+++ src/service/PedidoService.java
@@ -1,4 +1,44 @@
 package service;
 
+import model.Mesa;
+import model.Pedido;
+import model.enums.StatusMesa;
+import repository.MesaRepository;
+import repository.PedidoRepository;
+
 public class PedidoService {
+
+    private MesaRepository mesaRepository;
+    private PedidoRepository pedidoRepository;
+
+    public PedidoService(MesaRepository mesaRepository, PedidoRepository pedidoRepository) {
+        this.mesaRepository = mesaRepository;
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    public Pedido abrirPedido(int numeroMesa) {
+        Mesa mesa = mesaRepository.buscarPorNumero(numeroMesa);
+
+        if (mesa == null) {
+            return null;
+        }
+
+        if (mesa.getStatus() != StatusMesa.LIVRE) {
+            return null;
+        }
+
+        Pedido pedidoAtivo = pedidoRepository.buscarPedidoAtivoPorMesa(mesa.getId());
+
+        if (pedidoAtivo != null) {
+            return null;
+        }
+
+        int novoId = pedidoRepository.listarTodos().size() + 1;
+        Pedido novoPedido = new Pedido(novoId, mesa.getId());
+        mesa.setStatus(StatusMesa.OCUPADA);
+        pedidoRepository.salvar(novoPedido);
+
+        return novoPedido;
+    }
 }
+
