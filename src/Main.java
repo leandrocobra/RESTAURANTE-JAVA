32731d1a761653ebdad2b7c4b07fe27407b6929b
Leandro Cobra <leandroccobra@gmail.com>
2026-03-23 12:48:34 -0400

    estrutura inicial básica do main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
 
+import repository.*;
+import service.AutService;
+import service.CaixaService;
+import service.CozinhaService;
+import service.PedidoService;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Projeto Iniciado!");
 
+    public static void main(String[] args) {
+        ItemPedidoRepository itemPedidoRepository = new ItemPedidoRepository();
+        MesaRepository mesaRepository = new MesaRepository();
+        PedidoRepository pedidoRepository = new PedidoRepository();
+        ProdutoRepository produtoRepository = new ProdutoRepository();
+        UsuarioRepository usuarioRepository = new UsuarioRepository();
+
+        AutService autService = new AutService(usuarioRepository);
+        CaixaService caixaService = new CaixaService(pedidoRepository, mesaRepository, itemPedidoRepository);
+        CozinhaService cozinhaService = new CozinhaService(pedidoRepository);
+        PedidoService pedidoService = new PedidoService(mesaRepository, pedidoRepository,
+                                                        produtoRepository, itemPedidoRepository);
     }
 }
