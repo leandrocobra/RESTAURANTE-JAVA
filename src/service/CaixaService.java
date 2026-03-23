7a25340ecc0d81f0404e7c1dfe8b82af8d565bba
Leandro Cobra <leandroccobra@gmail.com>
2026-03-22 21:38:36 -0400

    estrutura inicial do caixa criada + serviço de listarpedidos à pagar

--- src/service/CaixaService.java
+++ src/service/CaixaService.java
@@ -1,4 +1,44 @@
 package service;
 
+import model.Pedido;
+import model.enums.StatusPagamentoPedido;
+import repository.ItemPedidoRepository;
+import repository.MesaRepository;
+import repository.PedidoRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CaixaService {
+    private PedidoRepository pedidoRepository;
+    private MesaRepository mesaRepository;
+    private ItemPedidoRepository itemPedidoRepository;
+
+    public CaixaService(PedidoRepository pedidoRepository, MesaRepository mesaRepository,
+                        ItemPedidoRepository itemPedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+        this.mesaRepository = mesaRepository;
+        this.itemPedidoRepository = itemPedidoRepository;
+    }
+
+    public List<Pedido> listarPedidosAguardandoPagamento(){
+        List<Pedido> todosPedidos = pedidoRepository.listarTodos();
+        List<Pedido> pedidosAguardandoPagamento = new ArrayList<>();
+
+        if (todosPedidos == null  || todosPedidos.isEmpty()){
+            return pedidosAguardandoPagamento;
+        }
+
+        for (Pedido pedido : todosPedidos){
+            if (pedido.getStatusPagamento() == StatusPagamentoPedido.FECHADO){
+                pedidosAguardandoPagamento.add(pedido);
+            }
+        }
+
+        return pedidosAguardandoPagamento;
+    }
+
+
+
+
 }
