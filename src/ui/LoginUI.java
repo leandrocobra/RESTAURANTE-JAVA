4388dfce8cafa356f48b46d476dd8285538ab233
Leandro Cobra <leandroccobra@gmail.com>
2026-03-24 16:54:04 -0400

    Método para fazer Login

--- src/ui/LoginUI.java
+++ src/ui/LoginUI.java
@@ -1,4 +1,23 @@
 package ui;
 
+import model.Usuario;
+import service.AutService;
+import java.util.Scanner;
+
 public class LoginUI {
+    private Scanner scanner;
+    private AutService autService;
+
+    public LoginUI(Scanner scanner, AutService autService) {
+        this.scanner = scanner;
+        this.autService = autService;
+    }
+
+    public Usuario fazerLogin(){
+        System.out.print("Usuário: ");
+        String usuario = scanner.nextLine();
+        System.out.print("Senha: ");
+        String senha = scanner.nextLine();
+        return autService.autenticar(usuario, senha);
+    }
 }
