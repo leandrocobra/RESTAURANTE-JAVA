b5809e50cb8c59e37890214b4b9753ab814ed8ee
Leandro Cobra <leandroccobra@gmail.com>
2026-03-24 18:52:58 -0400

    listar mesas e abrir pedido

--- src/ui/MenuConsole.java
+++ src/ui/MenuConsole.java
@@ -1,7 +1,10 @@
 package ui;
 
+import model.Mesa;
+import model.Pedido;
 import model.Usuario;
 import model.enums.PerfilUsuario;
+import model.enums.StatusMesa;
 import repository.MesaRepository;
 import repository.ProdutoRepository;
 import repository.UsuarioRepository;
@@ -35,23 +38,11 @@ public class MenuConsole {
         this.produtoRepository = produtoRepository;
     }
 
-    private void menuAdm(){
-        System.out.println("Menu ADM.");
-    }
-    private void menuGarcom(){
-        System.out.println("Menu GARCOM.");
-    }
-    private void menuCozinha(){
-        System.out.println("Menu COZINHA.");
-    }
-    private void menuCaixa(){
-        System.out.println("Menu CAIXA.");
-    }
-
     public void iniciar() {
         Usuario usuarioLogado = loginUI.fazerLogin();
         if (usuarioLogado == null) {
             System.out.println("Login inválido!");
+            return;
         } else {
             System.out.println("Login realizado com sucesso.");
             System.out.println("Bem vindo(a) " + usuarioLogado.getUsuario());
@@ -60,11 +51,70 @@ public class MenuConsole {
         PerfilUsuario perfilUsuario = usuarioLogado.getPerfil();
 
         switch (perfilUsuario){
-            case PerfilUsuario.ADM -> menuAdm();
-            case PerfilUsuario.GARCOM -> menuGarcom();
-            case PerfilUsuario.COZINHA -> menuCozinha();
-            case PerfilUsuario.CAIXA -> menuCaixa();
+            case ADM -> menuAdm();
+            case GARCOM -> menuGarcom();
+            case COZINHA -> menuCozinha();
+            case CAIXA -> menuCaixa();
         }
     }
 
+    private void menuAdm(){
+        System.out.println("Menu ADM.");
+    }
+    private void menuGarcom(){
+        int opcao;
+        do {
+            System.out.println("===Menu GARÇOM.===");
+            System.out.println("1 - Abrir pedido.");
+            System.out.println("2 - Adicionar item ao pedido.");
+            System.out.println("3 - Visualizar comanda.");
+            System.out.println("4 - Fechar pedido.");
+            System.out.println("0 - Sair");
+            System.out.println("Digite sua opção:");
+
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao){
+                case 1 -> abrirPedidoUI();
+                case 2 -> System.out.println("2 - Adicionar item ao pedido.");
+                case 3 -> System.out.println("3 - Visualizar comanda.");
+                case 4 -> System.out.println("4 - Fechar pedido.");
+                case 0 -> System.out.println("Saindo do menu GARÇOM ...");
+                default -> System.out.println("Opção inválida!\n");
+            }
+
+        }while (opcao != 0);
+
+    }
+    private void menuCozinha(){
+        System.out.println("Menu COZINHA.");
+    }
+
+    private void menuCaixa(){
+        System.out.println("Menu CAIXA.");
+    }
+
+    private void abrirPedidoUI(){
+        listarMesasLivres();
+        System.out.print("Informe o número da mesa: ");
+        int numeroMesa = scanner.nextInt();
+        scanner.nextLine();
+
+        Pedido pedido = pedidoService.abrirPedido(numeroMesa);
+        if (pedido == null){
+            System.out.println("Não foi possivel abrir o pedido");
+        }else {
+            System.out.println("Pedido aberto com sucesso:");
+            System.out.println("Pedido ID:" + pedido.getId() + "Mesa: " + numeroMesa);
+        }
+    }
+
+    private void listarMesasLivres(){
+        for (Mesa mesa : mesaRepository.listarTodos()){
+            if (mesa.getStatus() == StatusMesa.LIVRE){
+                System.out.println("Mesa " + mesa.getNumero());
+            }
+        }
+    }
 }
